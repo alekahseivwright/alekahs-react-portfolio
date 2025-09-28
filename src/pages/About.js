f9a1944a09d9d63d40b506c97823ee4ebae39725
AllytheCat <seivwrightalekah@gmail.com>
2025-09-27 22:17:31 -0400

    feat: add about page, headshot, resume, and css for page.

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import MeImg from "../assests/me.png";
+import ResumePdf from "../assets/Alekah_Seivwright_Resume.pdf";
+
+function About() {
+    return ( 
+        <div className="about"> 
+            <h1>About Me</h1>
+            <img src={MeImg} alt="Alekah Seivwright" className="about-img"/>
+            <p>
+               Hi, I am <strong>Alekah Seivwright</strong>. 
+               I am a student at Centennial College, and I am taking their Software Engineering Technician Program. 
+               I want to work as a project manager on an App or Game development team. 
+               I’m passionate about learning, creating, and growing as a student to achieve my goals in the tech world.
+               On my personal time, I enjoy weightlifting, gaming, and camping.
+              </p>
+              <a href= {ResumePdf} download className="btn">Download My Resume PDF
+              </a>
+        </div>
+     );
+}
+export default About;
