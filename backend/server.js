91ab37b112b752694773d67f43a0c2d7149e7717
AllytheCat <seivwrightalekah@gmail.com>
2025-11-18 23:15:04 -0500

    adding app.use for auth routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const projectRoutes = require("./routes/projectRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 // Initialize app
 const app = express();
@@ -10,12 +11,14 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Connect to MongoDB
 connectDB();
 
 // Routes
 app.use("/api/projects", require("./routes/projectRoutes"));
+app.use("/api/auth", authRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
