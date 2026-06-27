89c248f358f07abd2e9b02502afe5da47ead1204
AllytheCat <seivwrightalekah@gmail.com>
2026-06-27 14:13:15 -0400

    fix: allow deployed frontend to access backend API

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 const projectRoutes = require("./routes/projectRoutes");
 const authRoutes = require("./routes/authRoutes");
 const contactRoutes = require("./routes/contactRoutes");
-const educationRoutes = require("./routes/educationRoutes"); 
+const educationRoutes = require("./routes/educationRoutes");
 
 // Initialize app
 const app = express();
@@ -14,7 +14,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
 
 // Connect to MongoDB
 connectDB();
@@ -25,13 +24,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/education", educationRoutes);
 
+// Test route
 app.get("/", (req, res) => {
-    res.send("Server is running!");
+  res.send("Server is running!");
 });
-
 
 // Start Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
